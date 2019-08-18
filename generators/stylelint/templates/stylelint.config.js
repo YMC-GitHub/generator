bc4d20355ed3e8a2a9be5c3f65298721cdf85bf2
yemiancheng <ymc.github@gmail.com>
2019-08-18 15:40:52 +0800

    feat(stylelint): adding a stylelint config
      uses `yo yemiancheng:stylelint`

---
+++ generators/stylelint/templates/stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 设置规则
+  // rules: {},
+  // 设置插件
+  // "plugins": [],
+  // 设处理器
+  // processors: ['stylelint-my-processor'],
+  // 忽略文件
+  // ignoreFiles:[]
+  // 指定扩展
+  extends: ['stylelint-config-yemiancheng']
+};
