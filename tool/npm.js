9d25259475e25726f455da437f72edee3cffcacd
yemiancheng <ymc.github@gmail.com>
2019-08-23 17:55:04 +0800

    build(npm): adding some tools
      adds a `npm view` cmd

--- tool/npm.js
+++ tool/npm.js
@@ -3,7 +3,10 @@ const execa = require('execa');
 function config(opts) {
   return execa('npm', ['config', ...(opts || [])]);
 }
-
+function view(opts) {
+  return execa('npm', ['view', ...(opts || [])]);
+}
 module.exports = {
-  config
+  config,
+  view
 };
---
+++ tool/npmViewPkg.js
@@ -0,0 +1,26 @@
+/* eslint-env node */
+const { view } = require('./npm');
+const util = require('util');
+
+const main = pkg => view([pkg, '--json']).then((report) => {
+  // console.log(report.stdout);
+  const { name, homepage, repository } = JSON.parse(report.stdout);
+  const { url } = repository;
+  const authorReg = /\/\w{1,}\//ig;
+  let author = '';
+  if (util.isString(url)) {
+    author = url.match(authorReg)[0].split('/').filter(v => v).join('');
+  }
+  // console.log(name, homepage, author);
+  return {
+    name,
+    homepage,
+    author,
+    npmPage: `https://www.npmjs.com/package/${pkg}`
+  };
+}).catch(console.log);
+
+
+module.exports = main;
+// exmaple:
+// main('promisify-event');
