a9ff825e9d8bee092b2a21b1adf4d7b3e3d44cff
yemiancheng <ymc.github@gmail.com>
2019-08-28 20:57:21 +0800

    build(*): updating tool code
      supports note in file-list with `//`

--- tool/config-file-list.js
+++ tool/config-file-list.js
@@ -1,19 +1,8 @@
 const fileListStr = `
-tool/git-tag.js
-tool/git-tag-flow.js
+//tool/git-tag-flow.js
+//tool/git.js
 tool/config-file-list.js
-tool/config-repo.js
-tool/git-mv-config.js
-tool/git-push.js
-tool/git-remote.js
-tool/git-set-remote-origin.js
-tool/git-show-remote-origin.js
-tool/git-tag.js
-tool/npm-config-registry.js
-tool/npm-init.js
-tool/npm-list.js
-tool/shields.js
 `;
-const fileListArr = fileListStr.split('\n').filter(v => (v.trim() !== ''));
+const fileListArr = fileListStr.split('\n').filter(v => (v.trim() !== '' && !/^\/\//.test(v)));
 
 module.exports = fileListArr;
