1ebe106f4b9d2c0da011b6fdd08e72116c787735
yemiancheng <ymc.github@gmail.com>
2019-08-28 09:37:53 +0800

    build(*): updating tool code

--- tool/git-remote.js
+++ tool/git-remote.js
@@ -2,4 +2,7 @@
 /* eslint-env node */
 const { remote } = require('./git');
 const { logAsync } = require('./console');
-logAsync(remote([]));
+const repoConfig = require('./config-repo');
+let result;
+result = remote([], { cwd: repoConfig.dir });
+logAsync(result);
