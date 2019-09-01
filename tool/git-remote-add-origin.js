2ce9552be8b1dc3feb4e0d3998042994121c1828
yemiancheng <ymc.github@gmail.com>
2019-09-01 16:01:55 +0800

    build(tool): updating tool code

---
+++ tool/git-remote-add-origin.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+const { remote } = require('./git');
+const { logAsync } = require('./console');
+const repoConfig = require('./config-repo');
+
+const { GITHUB_REPO, GITHUB_USER } = repoConfig;
+logAsync(remote(['add', 'origin', `https://github.com/${GITHUB_USER}/${GITHUB_REPO}.git`], { cwd: repoConfig.dir }));
