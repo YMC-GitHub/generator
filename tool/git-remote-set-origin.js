8b99730b921cbc338dd7255c055ae961d0a2d9e9
yemiancheng <ymc.github@gmail.com>
2019-09-01 15:59:32 +0800

    build(tool): refatoring tool code

--- tool/git-remote-set-origin.js
+++ tool/git-remote-set-origin.js
@@ -3,6 +3,5 @@ const { remote } = require('./git');
 const { logAsync } = require('./console');
 const repoConfig = require('./config-repo');
 
-const GITHUB_REPO = 'eslint-config';
-const GITHUB_USER = 'ymc-github';
+const { GITHUB_REPO, GITHUB_USER } = repoConfig;
 logAsync(remote(['set-url', 'origin', `https://github.com/${GITHUB_USER}/${GITHUB_REPO}.git`], { cwd: repoConfig.dir }));
